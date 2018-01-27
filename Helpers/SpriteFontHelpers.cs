ac7faabb4be55ee2fe89150c9337e91fa12ebd7a
David Miles <quantumdave@gmail.com>
2018-01-27 16:04:17 +0000

    Add stages and fonts

---
+++ Helpers/SpriteFontHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Transmission.Helpers
+{
+    public class SpriteFontHelpers
+    {
+        public static void RenderTextWithWrapping(SpriteBatch batch, SpriteFont font, int lineHeight, string text, Rectangle rect) {
+
+            string textToRender = text;
+
+            int line = 0;
+
+            while (textToRender.Length > 0)
+            {
+                int c = 0;
+
+                while (c < textToRender.Length && font.MeasureString(textToRender.Substring(0, c)).X < rect.Width)
+                {
+                    c++;
+                }
+
+                if (c != 0)
+                {
+                    batch.DrawString(font, textToRender.Substring(0, c), new Vector2(40, 280 + (line * lineHeight)), Color.White);
+                }
+
+                line++;
+                textToRender = textToRender.Substring(c);
+            }
+        }
+    }
+}
---
+++ Scenes/ConvoScene.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json;
+using Transmission.Scenes.Story;
+
+namespace Transmission.Scenes
+{
+    public class ConvoScene : IScene
+    {
+        private IGame game = Transmission.Instance();
+        
+        private enum State {
+            AnimatingIn,
+            AnimatingOut,
+            Static
+        }
+
+        private int height;
+
+        private Convo convo;
+        private int segment;
+
+        private SpriteBatch sb;
+        private Texture2D speakerTexture;
+        private Rectangle speakerRectangle;
+
+        public ConvoScene(string filename)
+        {
+            convo = JsonConvert.DeserializeObject<Convo>(File.ReadAllText(filename));   
+
+            int screenWidth = game.GDM().GraphicsDevice.Viewport.Width;
+            int screenHeight = game.GDM().GraphicsDevice.Viewport.Height;
+
+            var sb = new SpriteBatch(game.GDM().GraphicsDevice);
+        }
+
+
+        public void Draw(float pSeconds)
+        {
+            sb.Begin();
+
+            sb.Draw(speakerTexture, speakerRectangle, Color.White);
+
+            sb.End();
+        }
+
+        public void Update(float pSeconds)
+        {
+            
+        }
+
+        private Texture2D texForSpeaker(Speaker speaker) {
+            return game.CM().Load<Texture2D>("pixel");
+        }
+
+        private string speakerName(Speaker speaker) {
+            switch (speaker){
+                case Speaker.Bird:
+                    return "Bird";
+                default: 
+                    return "Unknown";
+            }
+        }
+    }
+}
